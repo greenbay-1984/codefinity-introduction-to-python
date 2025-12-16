81616a0c28ea3ffce9ca1ba5336add9cc19a87bd
greenbay-1984 <>
2025-12-16 17:54:13 +0000

    Conditional Statements

---
+++ conditional_statements/conditional_statements/main.py
@@ -0,0 +1,8 @@
+# Input variables
+product_type = "Dairy"  
+day_of_week = "Wednesday"
+if product_type == "Fruits" and day_of_week=="Monday":
+    print("10% discount on Fruits today!")
+elif product_type=="Vegetables" and day_of_week=="Tuesday":
+    print()
+    
